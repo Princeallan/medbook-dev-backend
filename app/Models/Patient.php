1de88750f27abb5511fcc1b8a54210bedf6b9080
Allan Kirui <allankirui57@gmail.com>
2023-08-28 12:04:46 +0300

    project setup plus generated models

---
+++ app/Models/Patient.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'date_of_birth', 'gender_id', 'user_id'
+    ];
+
+    public function services(): BelongsToMany
+    {
+        return $this->BelongsToMany(Service::class, '');
+    }
+
+    public function gender(): BelongsTo
+    {
+        return $this->belongsTo(Gender::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
